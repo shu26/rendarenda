69e8c3d11b13045054c5d7320f79f43c061afd99
海法修平 <haifaxiuping@kaihoushuuhei-no-MacBook-Air.local>
2017-02-14 13:09:26 +0900

    Initial Commit

---
+++ Tekipaki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tekipaki
+//
+//  Created by 海法修平 on 2017/02/14.
+//  Copyright © 2017年 Swift-Beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
